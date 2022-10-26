924429085ed465e6260ba33e3094738fa9386bc7
Lavanyadeiva <111745928+Lavanyadeiva@users.noreply.github.com>
2022-10-26 19:01:02 +0530

    Update averagearray.java

--- src/array/averagearray.java
+++ src/array/averagearray.java
@@ -1,21 +1,20 @@
 package array;
 public class averagearray {
  public static void main(String[] args) {
-   int num[]=new int[7];
-   num[0]=10;
-   num[1]=20;
-   num[2]=30;
-   num[3]=40;
-   num[4]=50;
-   num[5]=60;
-   num[6]=70;
-   double average=0.00;
-   int sum=0;
-   //find the sum of these numbers
-   for(int i=0;i<6;i++)
-		    sum  = sum + num[i];
-		    average = (double)sum/6;
-		 System.out.println("Sum of six numbers: " +sum);
-		 System.out.println("Average of six numbers: " +average);
-}
+
+		   int[] numbers = {2, -9, 0, 5, 12, -25, 22, 9, 8, 12};
+		   int sum = 0;
+		   Double average;
+		   
+	 for (int number: numbers){
+		     sum += number;
+		   }
+		  
+    int arrayLength = numbers.length;
+
+		   average =  ((double)sum / (double)arrayLength);
+
+		   System.out.println("Sum = " + sum);
+		   System.out.println("Average = " + average);
+		 }
 }
