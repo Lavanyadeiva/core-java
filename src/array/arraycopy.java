790cb47296a717ec7cd125ca8d7fd3f8aa674f32
Lavanyadeiva <111745928+Lavanyadeiva@users.noreply.github.com>
2022-10-26 19:25:02 +0530

    Add files via upload

---
+++ src/array/arraycopy.java
@@ -0,0 +1,18 @@
+package array;
+import java.util.Arrays;
+
+class arraycopy {
+    public static void main(String[] args) {
+      
+        int [] source = {1, 2, 3, 4, 5, 6};
+        int [] destination = new int[6];
+
+        // iterate and copy elements from source to destination
+        for (int i = 0; i < source.length; ++i) {
+            destination[i] = source[i];
+        }
+      
+         // converting array to string by []and ,
+        System.out.println(Arrays.toString(destination));
+    }
+}
